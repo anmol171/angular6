f42eacd445169d2ba309d19f1f31a0ee06700d95
DESKTOP-9QI22M5\Polestar User <priyesh.bhangadia@polestarllp.com>
2018-07-13 14:00:50 +0530

    recipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,50 +19,7 @@
 
 
 
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-
-// export class AppComponent {
-
-// ASSIGNEMENT 3
-//  showSecret = false;
-//   log = [];
-//   onToggleDetails(){ 
-  //this.showSecret = !this.showSecret;
-  //this.log.push(this.log.length + 1);
-  //   }
-
-
-// servers = [];
-
-// onAddServer(){
-//   this.servers.push('Another Server');
-// }
-
-// onRemoveServer(id: number) {
-//   const position = id+1;
-//   const position = id;
-//   this.servers.splice(position, 1);
-// }
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -72,36 +29,79 @@ import { Component, Input } from '@angular/core';
 
 export class AppComponent {
 
+// //ASSIGNEMENT 3
+//  showSecret = false;
+//   log = [];
+//   onToggleDetails(){ 
+//   this.showSecret = !this.showSecret;
+//   this.log.push(this.log.length + 1);
+//     }
 
 
-@Input() serverElements = [{ type: 'server', name: 'Testserver', content : 'Just a Test!'}];
+// servers = [];
 
-onServerAdded(serverData : { serverName: string,serverContent:string }) {
-  this.serverElements.push({
-    type: 'server',
-    name: serverData.serverName,
-    content: serverData.serverContent
-  });
-}
+// onAddServer(){
+//   this.servers.push('Another Server');
+// }
 
-onBlueprintAdded( blueprintData : { serverName: string,serverContent:string } ) {
-  this.serverElements.push({
-    type: 'blueprint',
-    name: blueprintData.serverName,
-    content: blueprintData.serverContent
-  });
-}  
-
-onChangeFirst(){
-  this.serverElements[0].name='Changed!';
-}
-
-onDestroyFirst(){
-  this.serverElements.splice(0, 1);
-}
+// onRemoveServer(id: number) {
+//   //const position = id+1;
+//   //const position = id;
+//   this.servers.splice(position, 1);
+// }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+// import { Component, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+
+// export class AppComponent {
+
+
+
+// @Input() serverElements = [{ type: 'server', name: 'Testserver', content : 'Just a Test!'}];
+
+// onServerAdded(serverData : { serverName: string,serverContent:string }) {
+//   this.serverElements.push({
+//     type: 'server',
+//     name: serverData.serverName,
+//     content: serverData.serverContent
+//   });
+// }
+
+// onBlueprintAdded( blueprintData : { serverName: string,serverContent:string } ) {
+//   this.serverElements.push({
+//     type: 'blueprint',
+//     name: blueprintData.serverName,
+//     content: blueprintData.serverContent
+//   });
+// }  
+
+// onChangeFirst(){
+//   this.serverElements[0].name='Changed!';
+// }
+
+// onDestroyFirst(){
+//   this.serverElements.splice(0, 1);
+// }
+
+
+// }
 
 
 
