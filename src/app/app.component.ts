9d55f77edf5e7d94dc694c04b2da3e3872e804c8
DESKTOP-9QI22M5\Polestar User <priyesh.bhangadia@polestarllp.com>
2018-07-16 19:08:36 +0530

    uptolecture94

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,10 @@ export class AppComponent {
 
   numbers = [1, 2, 3, 4, 5];
   onlyOdd = false;
-  
+  value = 5;
   oddNumbers = [1, 3, 5];
   evenNumbers = [2, 4];
+  
 
 
 
--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,12 +1,39 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
     selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-    constructor(private element: ElementRef, private renderer: Renderer2) { }
+  @Input() defaultColor:string = 'transparent';
+  @Input('appBetterHighlight') highlightColor:string = 'blue';
+
+
+  //@HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+//  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
+@HostBinding('style.backgroundColor') backgroundColor: string;
+  
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
 
     ngOnInit() { 
-        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'blue');
+      this.backgroundColor = this.defaultColor;
+        // this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'blue');
     }
+
+    @HostListener('mouseenter') mouseover(eventData: Event){
+
+ //     this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'blue');
+      
+    //this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
+
+    }
+
+    @HostListener('mouseleave') mouseleave(eventData: Event){
+
+ //     this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'transparent');
+      //this.backgroundColor = 'transparent';
+      this.backgroundColor = this.defaultColor;
+   
+    }
+
 }
