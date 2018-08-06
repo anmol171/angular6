9e0754436fb3f59d9e19f839e218ac953af8a251
shubham <shubham.agarwal@polestarllp.com>
2018-08-06 19:56:30 +0530

    sdfsddf

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,11 +9,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
  recipeWasSelected = new EventEmitter<Recipe>();
+
+   recipes: Recipe[];
  
 
-  constructor() { }
+  constructor(private recipeService : RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe){
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,4 +9,8 @@ export class RecipeService {
         new Recipe('A Chicken Recipe','This is simply a test','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/6/8/5/FNM_070110-Weekend-Dinners-026_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371593415273.jpeg')
     
       ];
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
 }
